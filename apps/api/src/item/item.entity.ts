94738cf16a08da78e8a31af8675f24efdf8c182a
Alex <alexy4744@gmail.com>
2021-01-03 16:36:56 -0500

    refactor(api): use bigint to represent item size column

--- apps/api/src/item/item.entity.ts
+++ apps/api/src/item/item.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, OneToMany } from "typeorm";
 
-import { Exclude } from "class-transformer";
+import { Exclude, Transform } from "class-transformer";
+import { Max, Min } from "class-validator";
 
 import { BaseEntity } from "../common/entities/base.entity";
 
@@ -20,6 +21,14 @@ export class ItemEntity extends BaseEntity {
   @Column({ unique: true })
   hash!: string;
 
-  @Column()
+  /*
+   * Postgres integers has a maximum of 2147483647 bytes (~2 gb).
+   * Using bigint raises this limit to (2 ** 63) - 1, however we set a max to
+   * (2 ** 53) - 1 as it is the highest integer javscript can represent accurately
+   */
+  @Column({ type: "bigint" })
+  @Min(0)
+  @Max(Number.MAX_SAFE_INTEGER)
+  @Transform((value) => Number(value))
   size!: number;
 }
