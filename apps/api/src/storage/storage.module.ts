7b44536671b7eb9732745cfd4589c71e082d86d9
Alex <alexy4744@gmail.com>
2021-02-17 17:06:31 -0500

    refactor(api): remove unused config module in storage module

--- apps/api/src/storage/storage.module.ts
+++ apps/api/src/storage/storage.module.ts
@@ -1,12 +1,11 @@
 import { BullModule } from "@nestjs/bull";
-import { ConfigModule } from "@nestjs/config";
 import { Module } from "@nestjs/common";
 
 import { StorageProcessor } from "./storage.processor";
 import { StorageService } from "./storage.service";
 
 @Module({
-  imports: [BullModule.registerQueue({ name: "storage" }), ConfigModule],
+  imports: [BullModule.registerQueue({ name: "storage" })],
   exports: [BullModule, StorageService],
   providers: [StorageProcessor, StorageService]
 })
