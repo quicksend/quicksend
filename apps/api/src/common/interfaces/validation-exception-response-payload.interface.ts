43863dddc232b8424e925e284a7f246d71ab5026
Alex <alexy4744@gmail.com>
2021-04-12 14:41:02 -0400

    refactor(api): better typing for validation exception payloads

--- apps/api/src/common/interfaces/validation-exception-response-payload.interface.ts
+++ apps/api/src/common/interfaces/validation-exception-response-payload.interface.ts
@@ -1,7 +1,10 @@
 import { HttpExceptionResponsePayload } from "./http-exception-response-payload.interface";
 
+import { ValidationError } from "class-validator";
+
 export interface ValidationExceptionResponsePayload extends HttpExceptionResponsePayload {
   details: Array<{
+    children: ValidationError[];
     constraints: Record<string, unknown>;
     property: string;
   }>;
